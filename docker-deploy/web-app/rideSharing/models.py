9d84ea03734e1e0393dd15aeb89c987d936054a0
Hangming-Ye <Hangming_Ye@outlook.com>
2023-01-31 12:51:25 -0500

    nothing

--- docker-deploy/web-app/rideSharing/models.py
+++ docker-deploy/web-app/rideSharing/models.py
@@ -15,18 +15,3 @@ class User(models.Model):
     license_plate_num = models.CharField(max_length=8,null=True)
     special_info = models.TextField(null=True)
     max_passenger = models.IntegerField(null=True)
-
-<<<<<<< Updated upstream
-    def __str__(self):
-        return self.user_name
-=======
-# a driver is a user (inheritance)
-class Driver(User):
-    #???????length
-    vehicle_type = models.CharField(max_length=10)
-    license_plate_num = models.CharField(max_length=8)
-    special_info = models.TextField()
-    max_passenger = models.IntegerField()
-
-
->>>>>>> Stashed changes
