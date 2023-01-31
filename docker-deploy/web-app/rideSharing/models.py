525989a6370a1684c684d1676c3c41c833feb576
Hangming-Ye <Hangming_Ye@outlook.com>
2023-01-31 12:45:14 -0500

    modify user model

--- docker-deploy/web-app/rideSharing/models.py
+++ docker-deploy/web-app/rideSharing/models.py
@@ -11,13 +11,10 @@ class User(models.Model):
     password = models.CharField(max_length=256)
     is_driver = models.BooleanField()
 
+    vehicle_type = models.CharField(max_length=10, null=True)
+    license_plate_num = models.CharField(max_length=8,null=True)
+    special_info = models.TextField(null=True)
+    max_passenger = models.IntegerField(null=True)
+
     def __str__(self):
         return self.user_name
-
-# a driver is a user (inheritance)
-class Driver(User):
-    #???????length
-    vehicle_type = models.CharField(max_length=10)
-    license_plate_num = models.CharField(max_length=8)
-    special_info = models.TextField()
-    max_passenger = models.IntegerField()
