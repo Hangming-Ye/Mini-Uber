0917661c8e5db58ce9cb3c38c14a32f3e8e967c9
Hangming-Ye <Hangming_Ye@outlook.com>
2023-02-01 21:14:41 -0500

    add modify search and delete function for ride

--- docker-deploy/web-app/ride/models.py
+++ docker-deploy/web-app/ride/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rideSharing.models import User
 
 class Ride(models.Model):
     rideType = models.BooleanField()
@@ -6,7 +7,8 @@ class Ride(models.Model):
     ownerPassNum = models.IntegerField()
     arrivalTime = models.DateTimeField()
     dest = models.CharField(max_length=128)
-    carType = models.CharField(max_length=20)
+    carType = models.CharField(max_length=20, blank = True, null = True, default=None)
+    totalPassNum = models.IntegerField()
     
     specialRequest = models.CharField(blank = True, max_length=128, null = True, default=None)
     
@@ -14,5 +16,29 @@ class Ride(models.Model):
     
     driver = models.IntegerField(blank = True, null=True, default=None)
     shared = models.JSONField(blank = True, null=True, default=None)
+    
     def __str__(self) -> str:
         return "Ride ID: " + str(self.id) +" Ride Owner: " + str(self.owner)
+    
+    def addRidtoUser(rid, uid):
+        user_obj = User.objects.get(pk=uid)
+        rid_dict = user_obj.ride_list
+        if rid_dict is None:
+            rid_dict = {}
+        if rid_dict[rid]:
+            return False # already exist
+        rid_dict[str(rid)] = True
+        user_obj.ride_list = rid_dict
+        user_obj.save()
+        return True
+    
+    def rmRidfromUser(rid,uid):
+        user_obj = User.objects.get(pk=uid)
+        rid_dict = user_obj.ride_list
+        if rid in rid_dict:
+            del rid_dict[rid]
+            user_obj.ride_list = rid_dict
+            user_obj.save()
+            return True
+        else:
+            return False # not exist
--- docker-deploy/web-app/ride/views.py
+++ docker-deploy/web-app/ride/views.py
@@ -1,14 +1,12 @@
 from django.http import HttpResponse
-from django.http import Http404
 from django.shortcuts import *
 from .models import Ride
 from rideSharing.models import User
 import datetime
-import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http.multipartparser import MultiPartParser
 
-#未实现：根据身份返回，根据当前人数判断是否能添加，加入增加list,结束删除list,异常抛出处理,加入share时的重复与验证问题
+#未实现：根据身份返回，根据当前人数判断是否能添加，异常抛出处理，有效性判断 (driver, user, ride)
 
 # Create your views here.
 def index(request):
@@ -31,6 +29,7 @@ def addRide(request):
             return HttpResponse("data error")
         ride_obj = Ride.objects.create(**data)
         ride_obj.save()
+        Ride.addRidtoUser(ride_obj.rid, data.uid)
         return HttpResponse(ride_obj)
     return HttpResponse("method wrong")
 
@@ -38,18 +37,17 @@ def addRide(request):
 def modifyRide(request):
     if request.method == 'PUT':
         data = MultiPartParser(request.META, request, request.upload_handlers).parse()[0].dict()
-        print(data)
-        ride = get_object_or_404(Ride, pk = data['rid'])
+        ride = Ride.objects.get(pk = data['rid'])
         if int(data['status']) == 0 and ride.status == 0:
-            if 'share_id' in data and 'share_num' in data:
+            if 'share_num' in data:
+                share_dict = ride.shared
                 if ride.shared is None:
                     share_dict = {}
-                else:
-                    share_dict = ride.shared
-                share_dict[data['share_id']] = data['share_num']
+                share_dict[data['uid']] = data['share_num']
                 ride.shared = share_dict
                 ride.save()
-                return HttpResponse("success add")
+                Ride.addRidtoUser(ride.rid, data.uid)
+                return HttpResponse("success add shared")
             elif int(data['uid']) == ride.owner:
                 ride.rideType = data['rideType']
                 ride.ownerPassNum = data['ownerPassNum']
@@ -63,16 +61,51 @@ def modifyRide(request):
             ride.status=int(data['status'])
             ride.driver=int(data['uid'])
             ride.save()
+            Ride.addRidtoUser(ride.pk, ride.driver)
             return HttpResponse("add driver")
         elif int(data['status']) == 2 and ride.status == 1:
-            ride.status=int(data['status'])
-            # delete in the user rid list
-            return HttpResponse("send email")
-            ride.save()
+            if ride.driver == uid: 
+                ride.status=int(data['status'])
+                for uid in ride.shared.keys():
+                    Ride.rmRidfromUser(ride.pk, uid)
+                Ride.rmRidfromUser(ride.pk,ride.owner)
+                Ride.rmRidfromUser(ride.pk, ride.driver)
+                ride.save()
+                return HttpResponse("send email")
         HttpResponse("data error")
     return HttpResponse("method wrong")
 
+def SearchRideDriver(request):
+    if request.method == 'GET':
+        data = request.GET.dict()
+        driver_obj = User.objects.get(pk=data['uid'])
+        driver_ride_list = driver_obj.ride_list.keys()
+        result = Ride.objects.filter(carType = driver_obj.vehicle_type
+                            ).filter(totalPassNum__lte=driver_obj.max_passenger
+                            ).filter(status=0
+                            ).filter(specialRequest = driver_obj.special_info
+                            ).order_by('arrivalTime')
+        for rid in driver_ride_list:
+            result = result.exclude(pk=rid)
+        return result
+    else:
+        return HttpResponse("method wrong")
 
+def SearchRideSharer(request):
+    if request.method == 'GET':
+        data = request.GET.dict()
+        sharer_obj = User.objects.get(pk=data['uid'])
+        sharer_ride_list = sharer_obj.ride_list.keys()
+        result = Ride.objects.filter(carType = driver_obj.vehicle_type
+                            ).filter(totalPassNum__lte=driver_obj.max_passenger
+                            ).filter(status=0
+                            ).filter(specialRequest = driver_obj.special_info
+                            ).order_by('arrivalTime')
+        for rid in sharer_ride_list:
+            result = result.exclude(pk=rid)
+        return result
+    else:
+        return HttpResponse("method wrong")
 
 '''
 def cancel_ride(request):
